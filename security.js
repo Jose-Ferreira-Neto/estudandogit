881ec02adc9509a490fd2a039a90596cd1195b48
José Ferreira Neto <jose.ferreiranetotrb@gmail.com>
2021-11-06 20:37:30 -0300

    raiz V1

---
+++ security.js
@@ -0,0 +1,12 @@
+function myFunction(){
+ 	var x,y;
+	x = prompt("DIGITE SEU NOME");
+	y = prompt("DIGITE SUA SENHA");
+		if(x == "jose" && y == "jose"){
+			alert("Bem vindo, MESTRE");
+			}else{
+	alert("IMPOSTOR");
+	window.close();
+}
+	
+}
